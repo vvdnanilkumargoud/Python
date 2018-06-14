40d5fce392a635f9520ea1650520ffb2c0857c60
anilkgb <anilkumar.bollapu@vvdntech.in>
2018-06-14 15:53:17 +0530

    adding a search in list and Dictionary

--- search.py
+++ search.py
@@ -1,8 +1,19 @@
 #a is list
 a = [1,2,3,4,5]
 #It will search entire list whthere value is there or not
-if 6 not in a:
-    a.append(6)
-    print a
+check = raw_input("Enter which you want to search in a list : ")
+if check not in a:
+    a.append(check)
+    print "%s is not in the list we are adding into list" %check
+    print "This is the latest list : %s" %a
 else:
     print a
+
+#b is dictionary
+
+b = {'c':4,'d':5,'e':7}
+key = raw_input("Enter which key you want to search in a dictionary : ")
+if key in b:
+    print b[key]
+else:
+    print "There is no %s as key in Dictionary" %key
